29e4f8eda467e7c3b5e2c357b67754376ba5354e
Itziar <ialonso@lst.tfo.upm.es>
2022-02-22 17:39:38 +0100

    read file

--- delivery3/code.py
+++ delivery3/code.py
@@ -1,1 +1,11 @@
+import re
 
+def exercise1():
+    file = open("messages_syslog_class.txt")
+    list_lines = file.readlines()
+    print(list_lines[0])
+
+if __name__ == '__main__':
+
+    # Call method for Exercise 1
+    exercise1()
