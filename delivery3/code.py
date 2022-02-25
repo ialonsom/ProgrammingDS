1cfb68b269e58a00bf57381cc2ea3a1372bb83e6
rcarbajobenito <98690376+rcarbajobenito@users.noreply.github.com>
2022-02-25 10:46:59 +0100

    Add Exercise 3

--- delivery3/code.py
+++ delivery3/code.py
@@ -5,7 +5,6 @@ def exercise1():
     """
     Point 1: Compute the ranking of usernames according to number of
     login attempts
-
     """
 
     # Create a pattern to find the name of the user
@@ -16,15 +15,15 @@ def exercise1():
     Example of invalid attempt:
      - Invalid user yoyo from 106.52.116.101 port 53873
      - Failed password for invalid user yoyo from 106.52.116.101 port 53873 ssh2
-
     """
     pattern = re.compile("session opened for user (\w+\d*) by|Invalid user (\w+\d*) from|Failed password for invalid user (\w+\d*) from")
-    # Create a dictionary to find easier the name of the users
+
+    # Create a dictionary to store the user names and login attemps
     attempts = dict([])
     for line in open("messages_syslog_class.txt"):
         for match in re.finditer(pattern, line):
             try:
-                # If the user exits we plus one to the number of attemps
+                # If the user exists we add one to the number of attemps
 
                 # Check if the match is for the first component of the pattern (group 1)
                 if match.group(1) is not 'None':
@@ -36,7 +35,7 @@ def exercise1():
                 if match.group(3) is not 'None':
                     attempts[match.group(3)] += 1
             except:
-                # If the user doesn't exits we stored the new user
+                # If the user doesn't exit we store the new user
 
                 # Check if the match is for the first component of the pattern (group 1)
                 if match.group(1) is not 'None':
@@ -52,8 +51,58 @@ def exercise1():
     attempts_sorted=sorted(attempts.items(), key=lambda item: item[1], reverse=True)
 
     print(attempts_sorted)
+
+def exercise3():
+
+    """
+    Point 3: Obtain a Ranking of failed login attempts by source IP
+    """
+
+    # Create a pattern to find the name of the user
+
+    """
+    Example of invalid attempt:
+     - Invalid user yoyo from 106.52.116.101 port 53873
+     - Failed password for invalid user yoyo from 106.52.116.101 port 53873 ssh2
+    """
+
+    pattern = re.compile("Invalid user \w* from (\d*.\d*.\d*.\d*) | Failed password for invalid user \w* from (\d*.\d*.\d*.\d*)")
+
+    # Create a dictionary to store the user names and login attemps
+    attempts = dict([])
+
+    for line in open("messages_syslog_class.txt"):
+        for match in re.finditer(pattern, line):
+            try:
+                # If the user exists we add one to the number of attemps
+
+                # Check if the match is for the first component of the pattern (group 1)
+                if match.group(1) is not 'None':
+                    attempts[match.group(1)] += 1
+                # Check if the match is for the second component of the pattern (group 2)
+                if match.group(2) is not 'None':
+                    attempts[match.group(2)] += 1
+            except:
+                # If the user doesn't exit we store the new user
+
+                # Check if the match is for the first component of the pattern (group 1)
+                if match.group(1) is not 'None':
+                    attempts[match.group(1)] =  1
+
+                # Check if the match is for the second component of the pattern (group 2)
+                if match.group(1) is not 'None':
+                    attempts[match.group(2)] =  1
+
+
+    # We transform the dictionary into a sorted list of tuples
+    failed_attempts_sorted=sorted(attempts.items(), key=lambda item: item[1], reverse=True)
+
+    print(failed_attempts_sorted)
     
 if __name__ == '__main__':
 
     # Call method for Exercise 1
     exercise1()
+
+    # Call method for Exercise 1
+    exercise3()
